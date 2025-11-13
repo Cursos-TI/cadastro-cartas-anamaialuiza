7b87f4b53c41696faa8efed1be1247afeaad0ff0
anamaialuiza <anamaia.nave@gmail.com>
2025-11-13 19:10:06 +0000

    Introduzi o código para a inserção de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,17 +6,57 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-    printf("Desafio Super Trunfo\n");
-   
-
-
+      char estado [50];
+      char codigocidade [3];
+      char cidade [50]; 
+      int população;
+      float area;
+      float pib;
+      int pontosturisticos;
+  
+      
 
   // Área para entrada de dados
+      printf ("Digite o nome de um estado brasileiro\n");//Usuário deverá inserir o nome de um estado do Brasil
+      scanf ("%s", &estado);
+
+      printf("Digite o código da carta que representa a cidade\n");//Usuário deverá inserir o código da cidade
+      scanf("%s", &codigocidade);
+
+      printf("Digite o nome da cidade\n");//Usuário deverá inserir o nome da cidade
+      scanf("%s", &cidade);
+
+      printf("Digite a população total\n");//Usuário deverá inserir a população total
+      scanf("%d", &população);
+
+      printf("Digite o PIB\n");//Usuário deverá inserir o PIB
+      scanf("%f", &pib);
+
+      printf("Digite os pontos turísticos\n");//Usuário deverá inserir o número de pontos turísticos
+      scanf("%d", &pontosturisticos);
+
+      printf("Digite a área total\n");//Usuário deverá inserir a área total
+      scanf("%f", &area);
 
   // Área para exibição dos dados da cidade
-    
+      printf("Carta 1 - Supertrunfo\n");
+
+      printf("O estado é:%s", estado);
+
+      printf("O código da cidade é: %s", codigocidade);
+
+      printf("O nome da cidade é: %s", cidade);
+
+      printf("População total: %d", população);
+
+      printf("PIB: %f", pib);
+
+      printf("Pontos Turísticos:%d", pontosturisticos);
+
+      printf("Área total:%f", area);
 
 
+      
 
 return 0;
 } 
