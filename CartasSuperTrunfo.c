002b5c1307e2fc9494acd4a3e83c78662e02a818
anamaialuiza <anamaia.nave@gmail.com>
2025-11-21 10:14:21 +0000

    Criei o printf para dados da carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,57 +6,22 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-      char estado [50];
-      char codigocidade [3];
-      char cidade [50]; 
-      int população;
-      float area;
-      float pib;
-      int pontosturisticos;
-  
-      
+    char estado;
+    char codigocarta [5];
+    char nomecidade [50];
+    int população;
+    float area;
+    float pib;
+    int pontosturisticos;
+    
+//Área para introduzir os dados pelo usuário para CARTA 1
+  printf("Digite uma letra de 'A' a 'H':\n");// O usuário deverá digitar uma letra de A a H
+  scanf("%c", &estado);
 
-  // Área para entrada de dados
-      printf ("Digite o nome de um estado brasileiro\n");//Usuário deverá inserir o nome de um estado do Brasil
-      scanf ("%s", &estado);
-
-      printf("Digite o código da carta que representa a cidade\n");//Usuário deverá inserir o código da cidade
-      scanf("%s", &codigocidade);
-
-      printf("Digite o nome da cidade\n");//Usuário deverá inserir o nome da cidade
-      scanf("%s", &cidade);
-
-      printf("Digite a população total\n");//Usuário deverá inserir a população total
-      scanf("%d", &população);
-
-      printf("Digite o PIB\n");//Usuário deverá inserir o PIB
-      scanf("%f", &pib);
-
-      printf("Digite os pontos turísticos\n");//Usuário deverá inserir o número de pontos turísticos
-      scanf("%d", &pontosturisticos);
-
-      printf("Digite a área total\n");//Usuário deverá inserir a área total
-      scanf("%f", &area);
-
-  // Área para exibição dos dados da cidade
-      printf("Carta 1 - Supertrunfo\n");
-
-      printf("O estado é:%s", estado);
-
-      printf("O código da cidade é: %s", codigocidade);
-
-      printf("O nome da cidade é: %s", cidade);
-
-      printf("População total: %d", população);
-
-      printf("PIB: %f", pib);
-
-      printf("Pontos Turísticos:%d", pontosturisticos);
-
-      printf("Área total:%f", area);
+  printf("Digite um número de 1 a 4: \n");// O usuário deverá digitar um número de 1 a 4
+  scanf("%d", codigocarta);
 
 
-      
+  return 0;
 
-return 0;
-} 
+}
